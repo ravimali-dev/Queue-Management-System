1d0a6ddae8c1270e80a607d0a11d13367c88f0b4
Ravi Mali <ravimalakar091@gmail.com>
2026-06-29 13:06:47 +0530

    add responsive dashboard and improve UI components

---
+++ src/Component/Queue.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react'
+
+function Queue({customerData, statusChange,deletCustomor}) {
+   
+
+   
+  
+  return (
+    <div >
+      <h3>Current Queue</h3>
+      {customerData.map((customer) => {
+        return (
+          <div key={customer.id} className="customer-card">
+          <h3 >{customer.name}</h3>
+          <h4>{ customer.service}</h4>
+          <p>{customer.status}</p>
+          <button onClick={() => statusChange(customer.id) }>{customer.status === "waiting" ? "Serve" : "Complete"}</button>
+          <button onClick = {() => deletCustomor(customer.id)}  >Delet</button>
+        </div>
+        )
+        
+      })}
+
+     
+      
+    </div>
+  )
+}
+
+export default Queue
